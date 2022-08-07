c84c7427bb0ad880f6297213dbe24fb86c3b2972
Kousthubhbhat <93434418+Kousthubhbhat@users.noreply.github.com>
2022-08-07 20:22:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,4 +15,23 @@ Bot = Client(
 async def start(bot: Client, cmd):
     return await cmd.reply_text(START_MESSAGE)
 
+@bot.on_message(filters.command('rip') & filters.private)
+async def start(bot, message):
+    await message.reply(
+        f"**{message.chat.first_name}!**\n"
+        "**Sorry To Say This You Dont Have Any Subscription 😞\n Buy Subscription From @CR_0O0...**")
+
+@bot.on_message(filters.command('Myplan') & filters.private)
+async def start(bot, message):
+    await message.reply(
+        f"**{message.chat.first_name}!**\n"
+        "**Sorry To Say This You Dont Have Any Subscription 😞\n Buy Subscription From @CR_0O0...**")
+
+@bot.on_message(filters.command('rip') & filters.private)
+async def start(bot, message):
+    await message.reply(
+        f"**{message.chat.first_name}!**\n"
+        "**To Know About Plans Kindly Contact @CR_0O0... ☺️**")
+
+
 Bot.run()
